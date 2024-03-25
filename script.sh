dcefdd45c4cf28a575ca6fd344bca5cd73cdaa6f
Ismael <imoro-sa@student.42malaga.com>
2024-03-25 20:07:05 +0100

    script_class

---
+++ script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+i=1
+
+while [ $i -le $# ]; do
+    # Variables
+    name=${!i}
+    header_file="${name}.hpp"
+    class_file="${name}.cpp"
+    header=$(echo ${!i} | tr '[a-z]' '[A-Z]')
+#header    
+    cat <<EOF > "$header_file"
+#ifndef ${header}_HPP
+#define ${header}_HPP
+#include <iostream>
+
+class $name
+{	
+	public:
+		$name();
+		$name(const $name& rhs);
+		$name& operator=(const $name& rhs);
+		~$name();
+};
+
+#endif
+EOF
+#class
+    cat <<EOF > "$class_file"
+#include "${header_file}"
+
+${name}::${name}()
+{
+
+}
+
+${name}::${name}(const ${name}& rhs)
+{
+
+}
+
+${name}& ${name}::operator=(const ${name}& rhs)
+{
+
+}
+
+${name}::~${name}()
+{
+
+}
+EOF
+    ((i++))
+	done
